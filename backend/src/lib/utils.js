d4440861add364509c269c666e6634c3b766ec3e
Alia Haider <turabhaider786@outlook.com>
2025-06-19 19:17:07 +0530

    changes made to the utils.js file secure true

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
         maxAge: 30 * 24 * 60 * 60 * 1000,//milisec
         httpOnly: true,//cookie is only accessible by web server,prevent xxs attach
         sameSite: "None",//cookie is only sent in same site , csrf attack
-        secure: process.env.NODE_ENV !== "development"//cookie is only sent in https
+        secure: true//cookie is only sent in https
     });
 
     return token;
